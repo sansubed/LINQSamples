91857f08c835c58a40d907653ca872760e61204b
Santona Subedi <santona.subedi@fastsw.com>
2024-01-30 08:39:54 -0600

    display the top 5 large files in the particular directory with & without LINQ

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LinqSamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string path = @"C:\Users\ssubedi";
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+                Console.WriteLine("*************");
+                ShowLargeFilesWithLinq(path);
+            }
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+            //Alternatively, to the query
+
+            var query1 = new DirectoryInfo(path).GetFiles()
+                           .OrderByDescending(f => f.Length)
+                           .Take(5);
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        public class FileInfoComparer : IComparer<FileInfo>
+        {
+            public int Compare(FileInfo x, FileInfo y)
+            {
+                return y.Length.CompareTo(x.Length);
+            }
+        }
+    }
+}
